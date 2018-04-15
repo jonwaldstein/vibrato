26d2280795f7d73ae5112715dcf76763e6b03752
Jon Waldstein <jpwaldstein@gmail.com>
2018-04-15 15:04:34 -0400

    fix theme options

--- lib/callbacks/fields/ThemeOptions.php
+++ lib/callbacks/fields/ThemeOptions.php
@@ -3,12 +3,10 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-Container::make( 'theme_options', __( 'Theme Options', 'sage' ) )
+Container::make( 'theme_options', __( 'Website Settings', 'sage' ) )
     ->add_tab( __('Header'), array(
         Field::make( 'image', 'site_logo', 'Logo' )
-            ->set_value_type( 'url' ),
         Field::make( 'image', 'mobile_logo', 'Mobile Logo' )
-            ->set_value_type( 'url' )
     ))
     ->add_tab( __('Footer'), array(
         Field::make( 'textarea', 'footer_copy', 'Footer Copy' ),
