3c1b5fdda09b53428aff51585e9c3185e428b7de
Jon Waldstein <jpwaldstein@gmail.com>
2019-06-28 11:53:39 -0400

    add common polyfills

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue';
 import { store } from './store';
 import domReady from '@wordpress/dom-ready';
+import objectFitImages from 'object-fit-images';
+
+// Import polyfills
+import './polyfill.js';
+require('es6-promise').polyfill();
 
 // Import custom modules
 import App from'./modules/app.js';
@@ -24,6 +29,7 @@ domReady( function() {
   nav.hamburger_animate(".js-hamburger");
   nav.primary(".js-trigger-navigation",".js-navigation");
   nav.sub_menu(".menu-item-has-children");
+  objectFitImages();
 
   if (document.getElementById('#appPosts')){
     const appPosts = new Vue({ 
