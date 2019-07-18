2ce9c1318a54f9963bc494d2cd9371f98ff0c1f5
Jon Waldstein <jpwaldstein@gmail.com>
2019-07-18 16:17:10 -0400

    cleanup

--- base.php
+++ base.php
@@ -19,16 +19,14 @@ use ZGM\Vibrato\Classes\Setup;
       get_template_part('templates/banner');
     ?>
     <div class="wrap container mx-auto px-6 py-6 lg:py-6 lg:px-0">
-      <div class="content row">
-        <main class="main">
-          <?php include Wrapper::template_path(); ?>
-        </main><!-- /.main -->
-        <?php if (Setup::display_sidebar()) : ?>
-          <aside class="sidebar">
-            <?php include Wrapper::sidebar_path(); ?>
-          </aside><!-- /.sidebar -->
-        <?php endif; ?>
-      </div><!-- /.content -->
+      <main class="main">
+        <?php include Wrapper::template_path(); ?>
+      </main><!-- /.main -->
+      <?php if (Setup::display_sidebar()) : ?>
+        <aside class="sidebar">
+          <?php include Wrapper::sidebar_path(); ?>
+        </aside><!-- /.sidebar -->
+      <?php endif; ?>
     </div><!-- /.wrap -->
     <?php
       do_action('get_footer');
