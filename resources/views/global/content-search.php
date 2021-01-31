ba38e365cfc9922b0342eeb0b682ca866c7f81e7
Jon Waldstein <jpwaldstein@gmail.com>
2021-01-31 01:13:57 -0500

    refactor: rename view paths

--- resources/views/global/content-search.php
+++ resources/views/global/content-search.php
@@ -2,7 +2,7 @@
     <header>
         <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php if (get_post_type() === 'post') {
-            get_template_part('resources/views/entry-meta');
+            get_template_part('resources/views/partials/posts/entry-meta');
         } ?>
     </header>
     <div class="entry-summary">
