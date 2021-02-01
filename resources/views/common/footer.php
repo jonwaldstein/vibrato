5365981d24b650d2e8af7a9fefdcfb585de134a7
Jon Waldstein <jonwaldstein@JonWaldteinMini.fios-router.home>
2021-02-01 10:33:07 -0500

    feature: add layout and footer style

--- resources/views/common/footer.php
+++ resources/views/common/footer.php
@@ -1,5 +1,5 @@
-<footer class="bg-gray-500 w-full">
-    <div class="max-w-7xl mx-auto p-8 flex justify-between items-center">
+<footer class="relative bg-gray-500 w-full">
+    <div class="max-w-7xl p-8 grid grid-flow-row grid-cols-3 gap-4 justify-items-center">
         <?php dynamic_sidebar(Vibrato\ValueObjects\Sidebar::footer()->value); ?>
         <p class="text-white">&#169; <?= date('Y '); ?></p>
     </div>
--- resources/views/layout/base.php
+++ resources/views/layout/base.php
@@ -14,11 +14,11 @@ use Vibrato\ThemeSetup;
     get_template_part('resources/views/common/header');
     !is_front_page() && get_template_part('resources/views/common/banner');
     ?>
-    <div class="wrap h-screen">
+    <div class="wrap">
         <main>
-            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 h-screen">
                 <!-- Replace with your content -->
-                <div class="px-4 py-6 sm:px-0">
+                <div class="px-4 py-6 sm:px-0 h-auto">
                     <?php include TemplateWrapper::template_path(); ?>
                 </div>
                 <!-- /End replace -->
