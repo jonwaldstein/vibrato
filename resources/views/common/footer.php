852d8ade8aa785c245978c5cfdd90c3042182092
Jon Waldstein <jonwaldstein@JonWaldteinMini.fios-router.home>
2021-02-01 23:44:13 -0500

    refactor: use esc_html for footer text

--- resources/views/common/footer.php
+++ resources/views/common/footer.php
@@ -4,7 +4,13 @@
             <?php dynamic_sidebar(Vibrato\ValueObjects\Sidebar::footer()->value); ?>
         </div>
         <div class="flex justify-center items-center p-6 md:p-8">
-            <p class="text-gray-600 text-sm">Copyright &#169; <?= sprintf('%s %s', date('Y '), get_bloginfo('name')); ?> | All rights reserved.</p>
+            <p class="text-gray-600 text-sm">
+                <?php
+                echo esc_html('Copyright &#169;');
+                echo sprintf('%s %s', date('Y '), get_bloginfo('name'));
+                echo esc_html(' | All rights reserved.');
+                ?>
+            </p>
         </div>
     </div>
 </footer>
